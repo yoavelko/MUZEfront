7c9be2ab7af218a02d0634f13493018dc04f37b8
Yoavelko <yoavelkobi889@gmail.com>
2025-01-12 09:17:50 +0200

    socket-context

--- src/contexts/SocketContext.jsx
+++ src/contexts/SocketContext.jsx
@@ -3,10 +3,19 @@ import { io } from 'socket.io-client'
 export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
-  const socket = io(import.meta.env.DEV ? 'http://localhost:3001' : import.meta.env.VITE_SOCKET);
+
+  const socket = io(import.meta.env.DEV ? 'http://localhost:3001' : import.meta.env.VITE_SOCKET,
+    {
+      transports: ['websocket'], // Force WebSocket transport
+      withCredentials: true,    // Allow cross-origin requests if needed
+    }
+  );
+
   useEffect(() => {
     socket.on('connect', () => {
-      socket.emit('join-room', 'room-1')
+      console.log('socket connection try');
+      socket.emit('join-room', 'room-1');
+      console.log('socket emit try');
     })
   }, [])
   return (
